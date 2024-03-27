9df284acd7be243f16890bd4112872090d605d7d
Enioluwa <enioluwaarowo@gmail.com>
2024-03-27 08:47:27 +0100

    add services page

--- app/(landing)/(home)/_components/services.tsx
+++ app/(landing)/(home)/_components/services.tsx
@@ -1,10 +1,62 @@
 "use client";
 
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { servicesData } from "@/data/servicesData";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useState } from "react";
+
 const Services = () => {
+  const [index, setIndex] = useState(0);
+
   return (
-    <section className=" bg-white">
+    <section className=" bg-white text-black">
       <div className=" py-20 container flex flex-col gap-6">
-        <h3>Everything you need, less of what you don't.</h3>
+        <h2>
+          Everything you need,
+          <br />
+          less of what you don't.
+        </h2>
+        <div className=" flex flex-col lg:flex-row lg:items-center gap-20">
+          <div className=" flex flex-col lg:flex-1">
+            {servicesData.map((service, i) => (
+              <div
+                onClick={() => setIndex(i)}
+                key={i}
+                className={cn(
+                  " space-y-2 px-3 py-6 border-l-4 cursor-pointer",
+                  index === i ? "border-primary" : "border-[#F9FAFA]"
+                )}
+              >
+                <h3
+                  className={cn(
+                    " capitalize",
+                    index === i ? " text-black " : "text-[#8F96A3]"
+                  )}
+                >
+                  {service.title}
+                </h3>
+                <p
+                  className={cn(
+                    index === i ? " text-black " : "text-[#B4B8AE]"
+                  )}
+                >
+                  {service.subtitle}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className=" lg:flex-1 w-full hidden lg:block">
+            <AspectRatio ratio={1 / 1}>
+              <Image
+                src={servicesData[index].image}
+                alt="Image"
+                className="rounded-xl object-cover"
+                fill
+              />
+            </AspectRatio>
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ data/servicesData.ts
@@ -0,0 +1,32 @@
+export const servicesData = [
+  {
+    image: "/images/recent.png",
+    title: "payments",
+    subtitle: "Create instant, scheduled and recurring payments easily",
+    id: "0",
+  },
+  {
+    image: "/images/hero.png",
+    title: "invoices",
+    subtitle: "Send, receive and reconcile invoices all on one platform",
+    id: "1",
+  },
+  {
+    image: "/images/support.png",
+    title: "Cashflow Support",
+    subtitle: "Move your business forward with insight and financial resources",
+    id: "2",
+  },
+  {
+    image: "/images/testimonial.png",
+    title: "cross-platform availability",
+    subtitle: "Bank everywhere. Available on iOS, Android and Slack",
+    id: "3",
+  },
+  {
+    image: "/images/recent.png",
+    title: "team banking",
+    subtitle: "Work with your team to run your entire financial opertations",
+    id: "4",
+  },
+];
