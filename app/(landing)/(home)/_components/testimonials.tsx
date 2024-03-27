287a39cdf0d60a9f59780b75b7ef26249c01fb83
Enioluwa <enioluwaarowo@gmail.com>
2024-03-27 09:15:32 +0100

    add testimonials [WIP]

--- app/(landing)/(home)/_components/testimonials.tsx
+++ app/(landing)/(home)/_components/testimonials.tsx
@@ -6,19 +6,22 @@ import Image from "next/image";
 const Testimonials = () => {
   return (
     <section className=" bg-black">
-      <div className=" lg:container flex flex-col lg:flex-row">
-        <div className=" container py-10 space-y-6">
-          <h3>We are built for every ambitious business</h3>
+      <div className=" container flex flex-col lg:flex-row lg:items-center gap-10 lg:px-0">
+        <div className="  lg:flex-1 py-10 pl-4 lg:pl-8 space-y-6">
+          <h2>
+            We are built for every <br />
+            ambitious business
+          </h2>
           <p>
             Thousands of forward-thinking businesses rely on Brass everyday to
             turbo-charge their business financial operations
           </p>
-          <h5>
+          <h3 className="pt-24 font-light">
             “The ease of opening an account completely won me over. The app is
             very easy to navigate and it&apos;s obvious they have the best
             insterests of SMEs at heart. I&apos;m proud to call them my bank.”
-          </h5>
-          <p>Olakunbi Adedipe, Odu Fashion</p>
+          </h3>
+          <p className=" text-[#8F96A3] pt-4">Olakunbi Adedipe, Odu Fashion</p>
           <div>
             <svg
               width="12"
@@ -31,12 +34,12 @@ const Testimonials = () => {
             </svg>
           </div>
         </div>
-        <div className=" lg:flex-1 w-full max-w-xl h-[600px]">
+        <div className=" lg:flex-1 w-full h-full">
           <AspectRatio ratio={1 / 1}>
             <Image
-              src="/images/recent.png"
+              src="/images/testimonial.png"
               alt="Image"
-              className="rounded-lg object-cover"
+              className="object-cover"
               fill
             />
           </AspectRatio>
